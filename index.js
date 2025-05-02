f8a5ccd655f54a8d7eba9340531a1b0d12e88114
mcBorgar <borgarfb@gmail.com>
2025-05-02 12:33:32 +0200

    .env, gitignore, fjern biler 1 og 1

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+require('dotenv').config();
 const express = require('express');                     //koble på http
 const bodyParser = require('body-parser');              //lese json
 const mysql = require('mysql2');                        //koble til mysql
 const cors = require('cors');                           //tillater komm nett-api
 
+
 const app = express();                                  //lage express app
 app.use(cors());
 app.use(bodyParser.json());
 
-const db = mysql.createConnection({                     //koble til sql database
-  host: '192.168.6.164',
-  user: 'bob3',
-  password: 'passord',
-  database: 'bob_biler'
+const db = mysql.createConnection({                     //allt er i env filen
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
+  
 //https://www.w3schools.com/nodejs/nodejs_mysql.asp 
-
 
 
 db.connect(err => {                                     //console log ved feil
@@ -46,14 +48,30 @@ app.post('/leggtil', (req, res) => {    //venter på klient /leggtil sender navn
     });
   });  //vente på respones, res.send
   
-  app.post('/slett', (req, res) => {    //venter på klient /leggtil sender navn og pris json
+  //slette 
+  app.post('/slett', (req, res) => {
     const { navn } = req.body;
   
-    db.query('DELETE FROM biler WHERE navn = ?', [navn], err => {  //sletter alle biler (skal endres)
+    db.query('SELECT antall FROM biler WHERE navn = ?', [navn], (err, result) => {
       if (err) return res.status(500).send(err);
-      res.send('Bil slettet!');
+      if (result.length === 0) return res.status(404).send('Bil finnes ikke');
+  
+      const antall = result[0].antall;
+  
+      if (antall > 1) {
+        db.query('UPDATE biler SET antall = antall - 1 WHERE navn = ?', [navn], err => {
+          if (err) return res.status(500).send(err);
+          res.send('Antall redusert med 1');
+        });
+      } else {
+        db.query('DELETE FROM biler WHERE navn = ?', [navn], err => {
+          if (err) return res.status(500).send(err);
+          res.send('Bil fjernet (siste)');
+        });
+      }
     });
   });
+  
   
   app.get('/biler', (req, res) => {
     db.query('SELECT * FROM biler', (err, result) => {        //db.query send dette til sql
